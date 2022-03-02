c5eecf2908870aac1715dd839da53659a91c9248
shafanaura <shafanaura48@gmail.com>
2022-03-02 21:06:23 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Typography, Button } from "antd";
+import { useEffect, useState } from "react";
+import "./theme.dark.less";
+import "./theme.light.less";
+
+const { Content } = Layout;
+const { Title } = Typography;
+
+const themeTexts = {
+  light: {
+    titleText: "Good morning 🌤",
+    buttonText: "Change to dark",
+  },
+  dark: {
+    titleText: "Good night 🌔",
+    buttonText: "Change to light",
+  },
+};
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    }
+  }, [theme]);
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Content className="content">
+        <Title>{themeTexts[theme].titleText}</Title>
+        <Button type="primary" onClick={changeTheme}>
+          {themeTexts[theme].buttonText}
+        </Button>
+      </Content>
+    </Layout>
   );
 }
 
